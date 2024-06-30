b9f888d2395ee36e4305fb4adc953a647bb76225
ubaidi5 <ubaid7625@gmail.com>
2024-06-30 03:37:53 -0700

    create questions

---
+++ Test-1.js
@@ -0,0 +1,48 @@
+const products = [
+  {
+    id: 1,
+    title: "Essence Mascara Lash Princess",
+    category: "beauty",
+    price: 9.99,
+    tags: ["mascara"],
+    stock: 5,
+  },
+  {
+    id: 2,
+    title: "Eyeshadow Palette with Mirror",
+    category: "beauty",
+    price: 19.99,
+    tags: ["eyeshadow"],
+    stock: 44,
+  },
+  {
+    id: 3,
+    title: "Powder Canister",
+    category: "beauty",
+    price: 14.99,
+    tags: ["face powder"],
+    stock: 59,
+  },
+  {
+    id: 4,
+    title: "Red Lipstick",
+    category: "beauty",
+    price: 12.99,
+    tags: ["lipstick"],
+    stock: 68,
+  },
+  {
+    id: 5,
+    title: "Red Nail Polish",
+    category: "beauty",
+    price: 8.99,
+    tags: ["nail polish"],
+    stock: 71,
+  },
+];
+
+// Find the total price of all items.
+
+// Find the price of the procuct whose title is "Powder Canister"
+
+// Find what's the total cost of goods in the inventory
